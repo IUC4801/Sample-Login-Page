4f86e3dfd930bb35e6414b08160ed45829341b62
IUC4801 <61285140+IUC4801@users.noreply.github.com>
2020-10-13 17:46:56 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -50,7 +50,87 @@ if(isset($_POST['login']))
 }	
 ?>
 
+
+<?php
+
+// The version number (9_5_0) should match version of the Chilkat extension used, omitting the micro-version number.
+// For example, if using Chilkat v9.5.0.48, then include as shown here:
+include("chilkat_9_5_0.php");
+
+//  This example requires the Chilkat API to have been previously unlocked.
+//  See Global Unlock Sample for sample code.
+
+//  This example includes both client-side and server-side code.
+//  Each code segment is marked as client-side or server-side.
+//  Imagine these segments are running on separate computers...
+
+//  -----------------------------------------------------------------
+//  (Client-Side) Generate an ECC key, save the public part to a file.
+//  -----------------------------------------------------------------
+$prngClient = new CkPrng();
+$eccClient = new CkEcc();
+// privKeyClient is a CkPrivateKey
+$privKeyClient = $eccClient->GenEccKey('secp256r1',$prngClient);
+if ($eccClient->get_LastMethodSuccess() != true) {
+    print $eccClient->lastErrorText() . "\n";
+    exit;
+}
+
+// pubKeyClient is a CkPublicKey
+$pubKeyClient = $privKeyClient->GetPublicKey();
+$pubKeyClient->SavePemFile(false,'qa_output/eccClientPub.pem');
+
+//  -----------------------------------------------------------------
+//  (Server-Side) Generate an ECC key, save the public part to a file.
+//  -----------------------------------------------------------------
+$prngServer = new CkPrng();
+$eccServer = new CkEcc();
+// privKeyServer is a CkPrivateKey
+$privKeyServer = $eccServer->GenEccKey('secp256r1',$prngServer);
+if ($eccServer->get_LastMethodSuccess() != true) {
+    print $eccServer->lastErrorText() . "\n";
+    exit;
+}
+
+// pubKeyServer is a CkPublicKey
+$pubKeyServer = $privKeyServer->GetPublicKey();
+$pubKeyServer->SavePemFile(false,'qa_output/eccServerPub.pem');
+
+//  -----------------------------------------------------------------
+//  (Client-Side) Generate the shared secret using our private key, and the other's public key.
+//  -----------------------------------------------------------------
+
+//  Imagine that the server sent the public key PEM to the client.
+//  (This is simulated by loading the server's public key from the file.
+$pubKeyFromServer = new CkPublicKey();
+$pubKeyFromServer->LoadFromFile('qa_output/eccServerPub.pem');
+$sharedSecret1 = $eccClient->sharedSecretENC($privKeyClient,$pubKeyFromServer,'base64');
+
+//  -----------------------------------------------------------------
+//  (Server-Side) Generate the shared secret using our private key, and the other's public key.
+//  -----------------------------------------------------------------
+
+//  Imagine that the client sent the public key PEM to the server.
+//  (This is simulated by loading the client's public key from the file.
+$pubKeyFromClient = new CkPublicKey();
+$pubKeyFromClient->LoadFromFile('qa_output/eccClientPub.pem');
+$sharedSecret2 = $eccServer->sharedSecretENC($privKeyServer,$pubKeyFromClient,'base64');
+
+//  ---------------------------------------------------------
+//  Examine the shared secrets.  They should be the same.
+//  Both sides now have a secret that only they know.
+//  ---------------------------------------------------------
+print $sharedSecret1 . "\n";
+print $sharedSecret2 . "\n";
+
+?>
+
+
+
+
+
 </div>
 
 </body>
 </html>
+
